53418996ca3481bd6d2e366cb80c67a4ac735088
SejutiD <duttasejuti24@gmail.com>
2024-04-05 12:54:16 +0200

    made a card-like view in medium shape
    ran npm update

--- app/javascript/components/ProductDetail.jsx
+++ app/javascript/components/ProductDetail.jsx
@@ -6,35 +6,36 @@ const ProductDetail = ({ product, reviews }) => {
     reviews.reduce((sum, review) => sum + review.star, 0) / reviews.length;
 
   return (
-    <div className="p-24 h-screen bg-indigo-200">
-      <div className="flex flex-col">
+    <div className="min-h-screen bg-indigo-200 flex justify-center items-center">
+      <div className="p-8 bg-yellow-100 rounded-lg shadow-lg w-full md:w-3/4 lg:w-2/3 xl:w-1/2">
         <div className="mb-4">
           <h2 className="text-xl font-semibold">{product.name}</h2>
         </div>
         <div className="flex items-center mb-4">
-          <p className="text-gray-700">
+          <div className="flex items-center">
+            <p className="text-gray-700 mr-2"></p>
             <Rating value={averageRating.toFixed(1)} />
-          </p>
-        </div>
-        <p>
-          <button className="px-2 py-1 bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700 rounded">
-            Add Review
-          </button>
-        </p>
-        <div className="border-b border-blue-700 my-2"></div>
-      </div>
-
-      <div>
-        <h3 className="mt-4 text-mid font-semibold">Reviews:</h3>
-        {reviews.map((review) => (
-          <div
-            key={review.id}
-            className="border border-gray-300 rounded-md p-2 mt-1 flex justify-between items-center"
-          >
-            <p className="mr-2">{review.text}</p>
-            <Rating value={review.star} />
           </div>
-        ))}
+        </div>
+        <button className="px-2 py-1 bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700 rounded">
+          Add Review
+        </button>
+        <div className="border-b border-blue-700 my-2"></div>
+
+        <div>
+          <h3 className="mt-4 text-mid font-semibold">Reviews:</h3>
+          {reviews.map((review) => (
+            <div
+              key={review.id}
+              className="border border-gray-300 rounded-md p-2 mt-1 flex justify-between items-center"
+            >
+              <div className="flex items-center">
+                <p className="mr-2">{review.text}</p>
+                <Rating value={review.star} />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- app/javascript/components/Rating.jsx
+++ app/javascript/components/Rating.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { FaStar } from 'react-icons/fa';
+import React from "react";
+import { FaStar } from "react-icons/fa";
 
 const Rating = ({ value }) => {
-	const stars = [];
-	for (let i = 0; i < 5; i++) {
-			stars.push(
-					<FaStar
-							key = {i}
-							className={`star ${i < value ? 'text-yellow-500' : 'text-gray-400'}`}
-							size={15}
-					/>
-			)
-	}
+  const stars = [];
+  for (let i = 0; i < 5; i++) {
+    stars.push(
+      <FaStar
+        key={i}
+        className={`star ${i < value ? "text-yellow-500" : "text-gray-400"}`}
+        size={15}
+      />
+    );
+  }
 
-	return (
-			<div className="flex items-center">
-				{stars}
-				<span className="ml-2 normal-nums">{value}</span>
-			</div>
-	);
+  return (
+    <div className="flex items-center">
+      {stars}
+      <span className="ml-2 normal-nums">{value}</span>
+    </div>
+  );
 };
 
 export default Rating;
