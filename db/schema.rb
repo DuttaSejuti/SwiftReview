342ca0e9025662fc9be8cb8a3ce57e6229d6069b
SejutiD <duttasejuti24@gmail.com>
2024-04-09 23:35:10 +0200

    added validation to the models, changed the type of star to float
    checked from rails console that the validations and floating star rating is working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_200614) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_09_212325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_200614) do
   create_table "reviews", force: :cascade do |t|
     t.bigint "product_id", null: false
     t.text "text"
-    t.integer "star"
+    t.float "star"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_reviews_on_product_id"
